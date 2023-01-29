72d592ce2b57796196da23ce57d7f4746d7fd58b
Mariano Andolina <mariano.3gs@gmail.com>
2023-01-29 11:06:45 -0300

    1st commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+
+import os
+import csv
+import sqlite3
+
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+
+app = Flask(__name__)
+engine = sqlalchemy.create_engine("sqlite:///blog.db")
+base = declarative_base()
+
+@app.before_first_request
+def before_first_request_func():
+    # Crear aquí todas las bases de datos
+
+    class Post(base):
+        __tablename__ = 'post'
+
+        id = Column(Integer, primary_key=True)
+        username = Column(String)
+        titulo = Column(String)
+        texto = Column(String)
+
+    base.metadata.create_all(engine)
+    print("Base de datos generada")
+
+
+@app.route("/")
+def index():
+    return render_template('blog.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
+
